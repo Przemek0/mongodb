2d5a57fc8427ddbbe902fd9facf81425e0ce498c
Przemek <przemyslaw.piwonski@gft.com>
2021-10-19 08:39:20 +0200

    Minor changes

--- src/main/java/com/example/mongodb/PersonRepository.java
+++ src/main/java/com/example/mongodb/PersonRepository.java
@@ -1,8 +1,6 @@
 package com.example.mongodb;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends MongoRepository<Person, String> {
 }
